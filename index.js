f1646bb0d155465e637ee0087520ef8648342bb2
Brad Chaplin <40774429+bradchaplin@users.noreply.github.com>
2019-12-02 17:02:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 // When the user scrolls the page, execute myFunction
-//window.onscroll = function() {myFunction()};
+window.onscroll = function() {myFunction()};
 
 // Get the header //
-//var header = document.getElementById("myHeader");
-//var content = document.getElementById("myContent");
+var header = document.getElementById("myHeader");
+var content = document.getElementById("myContent");
 
 // Get the offset position of the navbar
-//var sticky = header.offsetTop;
-//var stickyPadding = content.offsetTop;
-// Add the sticky class to the header when you reach its scroll position.
+var sticky = header.offsetTop;
+var stickyPadding = content.offsetTop;
+//Add the sticky class to the header when you reach its scroll position.
 //Remove "sticky" when you leave the scroll position.
 //Add stickyPadding class to content class so that the padding under the header does not get affected.
 //Remove stickyPadding when not sticky and add content class back.
-//function myFunction() {
-  //if (window.pageYOffset > sticky) {
-    //header.classList.add("sticky");
-    //content.classList.add("stickyPadding");
- // } else {
-    //header.classList.remove("sticky");
-    //content.classList.remove("stickyPadding");
-    //content.classList.add("content");
- // }
-//} 
-
-
-<script>
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("myHeader");
-var sticky = myHeader.offsetTop;
-
 function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    myHeader.classList.add("sticky")
-  } else {
-    myHeader.classList.remove("sticky");
+  if (window.pageYOffset > sticky) {
+  	header.classList.add("sticky");
+    content.classList.add("stickyPadding");
+ } else {
+   	header.classList.remove("sticky");
+   	content.classList.remove("stickyPadding");
+   	content.classList.add("content");
   }
-}
-</script>
+} 
+
+
+//<script>
+//window.onscroll = function() {myFunction()};
+
+//var navbar = document.getElementById("myHeader");
+//var sticky = myHeader.offsetTop;
+
+//function myFunction() {
+ // if (window.pageYOffset >= sticky) {
+   // myHeader.classList.add("sticky")
+  //} else {
+    //myHeader.classList.remove("sticky");
+  //}
+//}
+//</script>
